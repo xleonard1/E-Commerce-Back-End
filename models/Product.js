4f96ebdb51b1bf3ac6fcbcd399a85138b1077a5f
xleonard1 <xavier.lnrd@gmail.com>
2022-05-28 13:32:41 -0400

    defined product columns

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
+const { Category } = require('.');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,6 +11,36 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      refrences: {
+        model: 'Category',
+        key: 'id'
+      }
+
+    }
   },
   {
     sequelize,
