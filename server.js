69a70b48b4e586ccf3806ca8daf6b8006c557216
xleonard1 <xavier.lnrd@gmail.com>
2022-05-28 12:50:57 -0400

    added files to workspace

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+
+sequilize.sync().then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+})
+
